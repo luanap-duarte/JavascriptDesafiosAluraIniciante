c106822209b8b7a904290853c787db9cdd3e46f4
Luana Duarte <suporte.luanaduarte@gmail.com>
2025-07-01 23:13:13 -0300

    commit do desafio alugames

---
+++ alugames/js/app.js
@@ -0,0 +1,15 @@
+function alterarStatus(id) {
+    let gameClick = document.getElementById(`game-${id}`);
+    let img = gameClick.querySelector('.dashboard__item__img');
+    let botao = gameClick.querySelector('.dashboard__item__button');
+
+    if(img.classList.contains('dashboard__item__img--rented')){
+        img.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else{
+        img.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
